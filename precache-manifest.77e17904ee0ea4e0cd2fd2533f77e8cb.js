80c6f6b476435894648f810167dd7e97fd6c5607
Denis V. Chumakov <den@10-13.ru>
2019-04-27 17:18:59 +0700

    'deploy'

---
+++ precache-manifest.77e17904ee0ea4e0cd2fd2533f77e8cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "37ef238973ea93daac91f1914478c40b",
+    "url": "/models/ssd_mobilenetv1_model-shard1"
+  },
+  {
+    "revision": "b6d5e81e2506145360be5c4278067080",
+    "url": "/models/ssd_mobilenetv1_model-shard2"
+  },
+  {
+    "revision": "f2091ed03625f6e164a637c2326691c1",
+    "url": "/models/face_recognition_model-shard2"
+  },
+  {
+    "revision": "cb6f0f62e7598d70acf76483185a962b",
+    "url": "/models/face_recognition_model-shard1"
+  },
+  {
+    "revision": "47047fee26557b55d985952bdfc6cba1",
+    "url": "/models/face_landmark_68_tiny_model-shard1"
+  },
+  {
+    "revision": "124304f06e07fcf928290ff776e96141",
+    "url": "/models/face_landmark_68_model-shard1"
+  },
+  {
+    "revision": "512265c057154d544ad2",
+    "url": "/js/chunk-vendors.962a48cb.js"
+  },
+  {
+    "revision": "0389ac012a2f482b9285",
+    "url": "/js/app.ee478e52.js"
+  },
+  {
+    "revision": "74ed4c7198d110daf5e38a332d800f41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0389ac012a2f482b9285",
+    "url": "/css/app.4ebdc48e.css"
+  }
+];
